110e1e4d0f3e13b7bcf681ea0454d082c19535c3
Orlando <muckyo@gmail.com>
2021-02-02 20:44:26 -0300

    add aula sobre numeros booleanos e null

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,5 +1,6 @@
 console.log('Bem vindos ao Starter!!!')
 
+console.log('Aula sobre String:')
 /**
  * String
  * 
@@ -9,7 +10,6 @@ console.log('Bem vindos ao Starter!!!')
  * '' aspas simples
  * `` template literals ou template strings
  */
-
 // implementação correta de caracteres 
 console.log('"Mayk"') 
 console.log(`Orlando 'Ramos' "Vilar" `)
@@ -21,3 +21,51 @@ console.log(1 + 1)// interpolação
 // console.log("Ma"yk") 
 // Uncaught SyntaxError: missing ) after argument list
 
+
+console.log('Aula sobre Number:')
+/**
+ * Number
+    * números
+    * 
+    * 33 // inteiros
+    * 12.5 // reais - float
+    * NaN // Not a Number
+    * Infinity // Infinito  
+ */
+console.log(22)
+console.log(-33)
+console.log(12.5)
+console.log(12.5 + 12)
+console.log(12.5 / "aaa")
+console.log(12.5 == Infinity)
+console.log(Infinity)
+// console.log(infinity) não entende infinity com i menúsculo 
+
+
+console.log('Aula sobre Boolean:')
+/**
+ * Boolean
+ * 
+    * somente 2 valores
+    * 
+        * true // verdadeiro
+        * falso // false
+ */
+console.log(true)
+console.log(false)
+console.log(true == false)
+
+
+console.log('Aula sobre undefined x null:')
+/**
+ * undefined
+    * dado indefinido
+    * 
+ * null
+    * nulo
+    * objeto que não possui nada dentro
+    * diferente de indefinido    
+ */
+console.log(undefined)
+console.log(null)
+console.log(undefined === null)
