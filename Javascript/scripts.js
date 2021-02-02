7fc246f1af21d6edbe7f8dd314cd48a30c70e2f3
Orlando <muckyo@gmail.com>
2021-02-02 20:57:38 -0300

    Aula sobre object e array

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -2,19 +2,19 @@ console.log('Bem vindos ao Starter!!!')
 
 console.log('Aula sobre String:')
 /**
- * String
- * 
- * Cadeia de caracteres
- * 
- * "" aspas duplas
- * '' aspas simples
- * `` template literals ou template strings
- */
+* String
+* 
+    * Cadeia de caracteres
+    * 
+    * "" aspas duplas
+    * '' aspas simples
+    * `` template literals ou template strings
+*/
 // implementação correta de caracteres 
-console.log('"Mayk"') 
-console.log(`Orlando 'Ramos' "Vilar" `)
-console.log(`Orlando ${10 + 17}`) // expressão de linguagem
-console.log(1 + 1)// interpolação 
+    console.log('"Mayk"') 
+    console.log(`Orlando 'Ramos' "Vilar" `)
+    console.log(`Orlando ${10 + 17}`) // expressão de linguagem
+    console.log(1 + 1)// interpolação 
 
 // implementação incorreta de caracteres
 // console.log(""Mayk"") 
@@ -24,14 +24,14 @@ console.log(1 + 1)// interpolação
 
 console.log('Aula sobre Number:')
 /**
- * Number
+* Number
     * números
     * 
     * 33 // inteiros
     * 12.5 // reais - float
     * NaN // Not a Number
     * Infinity // Infinito  
- */
+*/
 console.log(22)
 console.log(-33)
 console.log(12.5)
@@ -44,13 +44,13 @@ console.log(Infinity)
 
 console.log('Aula sobre Boolean:')
 /**
- * Boolean
- * 
-    * somente 2 valores
-    * 
-        * true // verdadeiro
-        * falso // false
- */
+* Boolean
+* 
+* somente 2 valores
+* 
+    * true // verdadeiro
+    * falso // false
+*/
 console.log(true)
 console.log(false)
 console.log(true == false)
@@ -58,14 +58,48 @@ console.log(true == false)
 
 console.log('Aula sobre undefined x null:')
 /**
- * undefined
+* undefined
     * dado indefinido
-    * 
- * null
+* 
+* null
     * nulo
     * objeto que não possui nada dentro
     * diferente de indefinido    
- */
+*/
 console.log(undefined)
 console.log(null)
 console.log(undefined === null)
+
+
+console.log('Aula sobre Object:')
+/**
+* Object
+    * objeto
+    * Propriedades / Atributos
+    * Funcionalidades / Métodos
+    * { propriedade: "valor" }
+* 
+*/
+console.log({
+    name: "Orlando",
+    idade: 27,
+    andar: function() {
+        console.log('andar')
+    }
+})
+
+
+console.log('Aula sobre Array:')
+/**
+* Array (Vetores)
+    * Uma lista
+    * Agrupamento de dados
+    * 
+    * ["Orlando", 27]
+*/
+console.log([
+    "Leite",
+    "Ovos",
+    2,
+    3
+])
